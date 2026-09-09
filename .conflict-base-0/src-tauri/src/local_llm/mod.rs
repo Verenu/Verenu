dd529e7deb1ba80b68822260f73f0d6d79333543
MONKE2525E <221282747+MONKE2525E@users.noreply.github.com>
2026-09-09 10:01:09 -0700

    Integrated worktree

--- .conflict-base-0/src-tauri/src/local_llm/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod binary;
-pub mod download;
-pub mod manager;
-pub mod model;
-pub mod runtime;
-
-pub use binary::LocalLlmRuntimeInfo;
-pub use manager::{LocalLlmManager, LocalLlmState};
-pub use model::LocalLlmModelInfo;
